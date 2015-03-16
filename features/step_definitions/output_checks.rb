3c406e43baa9b0fc8c4e230b59c9daeff73b4a2e
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-03-16 19:36:29 +0100

    all_stdout

--- features/step_definitions/output_checks.rb
+++ features/step_definitions/output_checks.rb
@@ -1,22 +1,22 @@
 Then(/^the output should be a set of strings$/) do
-  x = parse_output(all_output)
+  x = parse_output(all_stdout)
   x.is_a?(Set) && x.all? {|e| e.is_a?(String) }
 end
 
 Then(/^the output should be eql to "(.*?)"$/) do |expected|
-  a = parse_output(all_output)
+  a = parse_output(all_stdout)
   b = parse_output(expected)
   expect(a).to be_meql(b)
 end
 
 Then(/^the output should be eql to:$/) do |expected|
-  a = parse_output(all_output)
+  a = parse_output(all_stdout)
   b = parse_output(expected)
   expect(a).to be_meql(b)
 end
 
 Then(/^the output should be eql to one of:$/) do |set_of_poss|
-  o = parse_output(all_output)
+  o = parse_output(all_stdout)
   s = parse_output(set_of_poss)
   expect(o).to be_included_with_meql(s)
 end
