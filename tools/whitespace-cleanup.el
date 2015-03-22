9450f4714ec4c2b19edf4633b74217c31b27fc1d
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-03-22 13:58:22 +0100

    also indent-region

--- tools/whitespace-cleanup.el
+++ tools/whitespace-cleanup.el
@@ -2,11 +2,18 @@
 ":"; exec emacs --no-site-file --script "$0" -- "$@" # -*-emacs-lisp-*-
 (setq argv (cdr argv))
 
+(load "~/.emacs")
+
+(defvar dont-indent nil)
+
 (defun cleanup-file (file)
   (message "Opening %s..." file)
   (find-file file)
-  (whitespace-cleanup)
-  (save-buffer)
+  (unless (member major-mode '(diff-mode))
+    (unless dont-indent
+      (indent-region (point-min) (point-max) nil))
+    (whitespace-cleanup)
+    (save-buffer))
   (kill-buffer))
 
 (custom-set-default 'indent-tabs-mode nil)
