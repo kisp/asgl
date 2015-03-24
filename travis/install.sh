5ac18d966fb6127474f3bf086517cb36fb5fa5b6
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-03-24 14:16:21 +0100

    sudo rm /usr/local/include/gecode/gist.hh

--- travis/install.sh
+++ travis/install.sh
@@ -46,6 +46,7 @@ function dist {
     sudo tar -C / -xjf "gecode.tar.bz2"
     rm gecode.tar.bz2
     sudo rm /usr/local/lib/libgecodegist*
+    sudo rm /usr/local/include/gecode/gist.hh
 
     curl --no-progress-bar --retry 10 -o "ecl.tar.gz" -L "http://178.62.230.106/packages/ecl-13.5.1-amd64.tgz"
     md5sum ecl.tar.gz
