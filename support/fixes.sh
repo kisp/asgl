67e644337096b672f4fed4e56425d07b3967d061
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-03-24 02:46:58 +0100

    fixes.sh

--- support/fixes.sh
+++ support/fixes.sh
@@ -2,5 +2,9 @@
 
 set -e
 
-exit 1
-
+rm data/iccma15_testcases/rdm1000_0.apx
+rm data/iccma15_testcases/rdm1000_1.apx
+rm data/iccma15_testcases/rdm1000_2.apx
+rm data/iccma15_testcases/rdm1000_3.apx
+rm data/iccma15_testcases/rdm1000_4.apx
+rm data/iccma15_testcases/rdm1000_5.apx
