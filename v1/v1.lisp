22deb6f980f959fb67a6ff6e0c55c6075c3e568b
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-03-25 22:09:46 +0100

    more tests

--- v1/v1.lisp
+++ v1/v1.lisp
@@ -611,11 +611,55 @@ res = 7;
 
 (myam:deftest test.3
   (check-extensions
-   "EE-ST" "[[a2x],[a1]]"
+   "EE-ST" "[[a2],[a1]]"
    '("arg(a1)."
      "att(a1, a2)."
      "arg(a2)."
      "att(a2,a1).")))
+
+(myam:deftest test.4
+  (check-extensions
+   "EE-CO" "[[a,b,d]]"
+   '("arg(a)."
+     "arg(b)."
+     "arg(c)."
+     "arg(d)."
+     "att(a, c)."
+     "att(b, c)."
+     "att(c, d).")))
+
+(myam:deftest test.5
+  (check-extensions
+   "EE-PR" "[[b,d],[b,c],[a,d],[a,c]]"
+   '("arg(a)."
+     "arg(b)."
+     "arg(c)."
+     "arg(d)."
+     "att(a, b)."
+     "att(b, a)."
+     "att(c, d)."
+     "att(d, c).")))
+
+(myam:deftest test.6
+  (check-extensions
+   "EE-CO" "[[a,b,c,d,f]]"
+   '("arg(a)."
+     "arg(b)."
+     "arg(c)."
+     "arg(d)."
+     "arg(e)."
+     "arg(f)."
+     "att(a, e)."
+     "att(b, e)."
+     "att(c, e)."
+     "att(d, e)."
+     "att(e, f).")))
+
+(myam:deftest test.7
+  (check-extensions
+   "EE-ST" "[]"
+   '("arg(a)."
+     "att(a, a).")))
 
 (defun main ()
   (setq *debugger-hook* (lambda (c old)
