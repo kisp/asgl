62d7a2b3f5ecad2ee3237db83ae74201607f2cc8
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-03-26 08:37:10 +0100

    echo env

--- travis/install.sh
+++ travis/install.sh
@@ -63,7 +63,10 @@ EOF
     chmod +x tmpecl/bin/ecl
     rm ecl.tar.gz
     ls `pwd`/tmpecl/lib
-    export ECL_R_SYS_DIR
+    echo env PATH=`pwd`/tmpecl/bin:$PATH \
+        LD_LIBRARY_PATH=`pwd`/tmpecl/lib \
+        ECL_R_SYS_DIR=`pwd`/tmpecl/lib/ecl-13.5.1 \
+        bash scripts/generate-make-mk.sh
     env PATH=`pwd`/tmpecl/bin:$PATH \
         LD_LIBRARY_PATH=`pwd`/tmpecl/lib \
         ECL_R_SYS_DIR=`pwd`/tmpecl/lib/ecl-13.5.1 \
