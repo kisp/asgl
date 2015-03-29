b81a3638d480150d25a81310b7fc86456c1eb103
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-03-29 14:52:49 +0200

    collect-timings / make-problem-set

--- lisp-scripts/collect-timings.lisp
+++ lisp-scripts/collect-timings.lisp
@@ -1,10 +1,44 @@
 #!/usr/bin/sbcl --script
 
+(defvar *start*)
+(defvar *end*)
+(defvar *count*)
+
+(defstruct benchmark input-file problem)
+
 (defun benchmark-files ()
   (sort (directory "data/**/*.apx") #'string< :key #'file-namestring))
 
-(defun run-benchmark (input-file problem &key (timeout "1s"))
-  (let* ((start (get-internal-real-time))
+(defun all-benchmarks ()
+  (let (list)
+    (dolist (file (benchmark-files))
+      (dolist (problem '(:ee-gr :se-gr
+                         :ee-co :se-co
+                         :ee-st :se-st
+                         :ee-pr :se-pr))
+        (let ((benchmark (make-benchmark :input-file file :problem problem)))
+          (push benchmark list))))
+    (nreverse list)))
+
+(defun benchmarks-from-list (benchmarks-list benchmarks-dir)
+  (with-open-file (input benchmarks-list)
+    (loop
+      for line = (read-line input nil)
+      while line
+      collect
+      (with-input-from-string (in line)
+        (make-benchmark
+         :problem (intern (read in) "KEYWORD")
+         :input-file
+         (let ((pathname (merge-pathnames (parse-namestring (read in))
+                                          benchmarks-dir)))
+           (or (probe-file pathname)
+               (error "missing input file ~A" pathname))))))))
+
+(defun run-benchmark (benchmark &key (timeout "1s"))
+  (let* ((input-file (benchmark-input-file benchmark))
+         (problem (benchmark-problem benchmark))
+         (start (get-internal-real-time))
          (process
            (sb-ext:run-program
             "timeout"
@@ -25,11 +59,11 @@
           (parse-integer version :start (1+ first-dot) :end second-dot)
           (parse-integer version :start (1+ second-dot)))))
 
-(defun timings-file-name (version timeout repeats)
+(defun timings-file-name (version timeout)
   (merge-pathnames
    (make-pathname
-    :name (format nil "timings-~A-r~A-~{~3,'0D~^-~}"
-                  timeout repeats
+    :name (format nil "timings-~A-~{~3,'0D~^-~}"
+                  timeout
                   (read-version version))
     :type "txt")
    "timings/"))
@@ -40,40 +74,73 @@
           (mod (truncate dur 60) 60)
           (mod dur 60)))
 
-(defvar *start* (get-universal-time))
-(format t ";start benchmarking~%")
-(sb-int:format-universal-time t *start*)
-(terpri)
 
-(destructuring-bind (timeout version &optional (repeats "1"))
-    (cdr sb-ext:*posix-argv*)
-  (let ((repeats (parse-integer repeats)))
-    (with-open-file (output (timings-file-name version timeout repeats)
-                            :direction :output
-                            :if-exists :error)
-      (format output "    file problem time exit-status~%")
-      (let ((count 0))
-        (dolist (file (benchmark-files))
-          (format t ";benchmarking ~A~%" file)
-          ;; cache warmup
-          (run-benchmark file "grounded" :timeout timeout)
-          (dotimes (i repeats)
-            (dolist (problem '(:ee-gr :se-gr
-                               :ee-co :se-co
-                               :ee-st :se-st
-                               :ee-pr :se-pr))
-              (multiple-value-bind (time exit-status)
-                  (run-benchmark file problem :timeout timeout)
-                (format output "~D ~A ~A ~A ~A~%"
-                        (incf count)
-                        (file-namestring file)
-                        problem
-                        time
-                        exit-status)
-                (finish-output output)))))))))
+(defun echo-start-benchmarking ()
+  (setq *start* (get-universal-time))
+  (format t ";start benchmarking~%")
+  (sb-int:format-universal-time t *start*)
+  (terpri))
 
-(defvar *end* (get-universal-time))
-(format t ";done benchmarking (~A)~%"
-        (format-dur (- *end* *start*)))
-(sb-int:format-universal-time t *end*)
-(terpri)
+(defun echo-done-benchmarking ()
+  (setq *end* (get-universal-time))
+  (format t ";done benchmarking (~A)~%"
+          (format-dur (- *end* *start*)))
+  (sb-int:format-universal-time t *end*)
+  (terpri))
+
+(defun echo-benchmarking-file (file)
+  (format t ";benchmarking ~A~%" file))
+
+(defun run-and-output-benchmark (benchmark timeout output)
+  (echo-benchmarking-file (benchmark-input-file benchmark))
+  ;; cache warmup
+  (run-benchmark benchmark :timeout "0.4s")
+  (multiple-value-bind (time exit-status)
+      (run-benchmark benchmark :timeout timeout)
+    (format output "~D ~A ~A ~A ~A~%"
+            (incf *count*)
+            (file-namestring (benchmark-input-file benchmark))
+            (benchmark-problem benchmark)
+            time
+            exit-status)
+    (finish-output output)))
+
+(defun adopt-keywords (list)
+  (mapcar (lambda (x)
+            (if (char= #\- (char x 0))
+                (intern (string-upcase (subseq x 1)) "KEYWORD")
+                x))
+          list))
+
+(defun main ()
+  (let ((args (cdr sb-ext:*posix-argv*)))
+    (destructuring-bind #1=(&key help
+                                 (timeout "10s")
+                                 (version "0.0.0")
+                                 (results-file (timings-file-name version timeout))
+                                 overwrite-results
+                                 benchmarks-list
+                                 (benchmarks-dir "data/iccma15_testcases/"))
+      (adopt-keywords args)
+      (cond
+        ((or (null args) help)
+         (format t "Usage: collect-timings ~S~%" '#1#)
+         (sb-ext:exit :code 1))
+        (t
+         (echo-start-benchmarking)
+         (with-open-file (output results-file
+                                 :direction :output
+                                 :if-exists (if overwrite-results
+                                                :supersede
+                                                :error))
+           (format output "    file problem time exit-status~%")
+           (let ((*count* 0))
+             (dolist (benchmark (if benchmarks-list
+                                    (benchmarks-from-list
+                                     benchmarks-list
+                                     (merge-pathnames benchmarks-dir))
+                                    (all-benchmarks)))
+               (run-and-output-benchmark benchmark timeout output))))
+         (echo-done-benchmarking))))))
+
+(main)
--- tools/timings/make-problem-set.R
+++ tools/timings/make-problem-set.R
@@ -2,6 +2,11 @@
 
 rep = function(d) { sprintf("file contains %d rows", nrow(d)) }
 
+# timeout in ms
+timeout = 1000 * 8.5
+# num of random samples
+rsamp = 12
+
 args <- commandArgs(trailingOnly = TRUE)
 inputFile = args[1]
 outputFile = args[2]
@@ -10,8 +15,7 @@ print(c(inputFile, outputFile))
 if (is.na(inputFile)) quit(status = 1)
 if (is.na(outputFile)) quit(status = 1)
 
-cutoff = 1000 * 8
-sprintf("timeout is %fs", cutoff / 1000)
+sprintf("timeout is %fs", timeout / 1000)
 
 sprintf("reading from file %s", inputFile)
 d = read.table(inputFile)
@@ -21,16 +25,29 @@ d = d[d$exit.status == 0,]
 sprintf("removing rows of exit.status not 0")
 rep(d)
 
-rsamp = 25
+d = d[d$time >= 500 & d$time <= 10000,]
+sprintf("keeping rows of time in [500ms <= time <= 10s]")
+rep(d)
+
 sprintf("taking a random sample of %d rows", rsamp)
 d = d[sample(1:nrow(d), rsamp),]
 rep(d)
 
-d = d[d$time < (cutoff*2),]
-sprintf("removing rows of time < %f", (cutoff*2)/1000.0)
+d = d[d$time < (timeout*2),]
+sprintf("removing rows of time < %f", (timeout*2)/1000.0)
 rep(d)
 
-sprintf("time to wait is %3.1fs", sum(pmin(cutoff, d$time))/1000.0)
+wait = sum(pmin(timeout, d$time))
+wait_warmup = nrow(d) * 400
+wait_total = wait + wait_warmup
+sprintf("expected run time is %3.1fs (%3.1fs + %3.1fs cache warmup)",
+  wait_total/1000.0, wait/1000.0, wait_warmup/1000.0)
 
-write.table(d[,c("problem", "file")], file = outputFile, quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(d[,c("problem", "file")], file = outputFile, quote = TRUE, row.names = FALSE, col.names = FALSE)
 sprintf("problem set written to %s", outputFile)
+
+write.table(
+  sprintf(
+    "./lisp-scripts/collect-timings.lisp -benchmarks-list %s -timeout %ss -results-file /tmp/r -overwrite-results t",
+      outputFile, timeout/1000.0),
+  quote = FALSE, row.names = FALSE, col.names = FALSE)
