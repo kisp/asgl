d433998cbc82046e5cdbc09ca59b9d2f17591793
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-09-02 16:55:08 +0200

    Removed unneeded files from lib/arnesi-list-match and lib/myam.

--- lib/myam/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sbcl --eval '(asdf:load-system :arnesi)' \
-     --eval "(asdf:oos 'arnesi:clean-op :myam)" \
-     --eval "(asdf:oos 'arnesi:clean-op :myam-test)" \
-     --eval '(asdf:test-system "myam")' \
-     --eval '(quit)'
