aed8040a29eee0e2cdc11a5efe70be196ebff38c
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-09-18 12:24:09 +0200

    Added features/comb-ee.feature.

--- features/step_definitions/official.rb
+++ features/step_definitions/official.rb
@@ -6,6 +6,14 @@ Given(/^official testcase "(.*?)"$/) do |name|
   copy_to_tmp_auba("data/iccma15_solutions/#{name}.EE-PR")
 end
 
+Given(/^apx testcase "(.*?)"$/) do |name|
+  copy_to_tmp_auba("data/apx/#{name}.apx")
+  copy_to_tmp_auba("data/apx/#{name}.EE-GR")
+  copy_to_tmp_auba("data/apx/#{name}.EE-CO")
+  copy_to_tmp_auba("data/apx/#{name}.EE-ST")
+  copy_to_tmp_auba("data/apx/#{name}.EE-PR")
+end
+
 Then(/^the output should be eql to solution "(.*?)"$/) do |name|
   path = "tmp/aruba/#{name}"
   raise "#{path} does not exit" unless File.exist?(path)
