b8f091ce96ff285428f0c37d3ed86ceecd7563bb
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-09-20 16:36:53 +0200

    cucumber assert_newline_terminated

--- features/step_definitions/output_checks.rb
+++ features/step_definitions/output_checks.rb
@@ -1,21 +1,25 @@
 Then(/^the output should be a set of strings$/) do
+  assert_newline_terminated(all_stdout)
   x = parse_output(all_stdout)
   x.is_a?(Set) && x.all? {|e| e.is_a?(String) }
 end
 
 Then(/^the output should be eql to "(.*?)"$/) do |expected|
+  assert_newline_terminated(all_stdout)
   a = parse_output(all_stdout)
   b = parse_output(expected)
   expect(a).to be_meql(b)
 end
 
 Then(/^the output should be eql to:$/) do |expected|
+  assert_newline_terminated(all_stdout)
   a = parse_output(all_stdout)
   b = parse_output(expected)
   expect(a).to be_meql(b)
 end
 
 Then(/^the output should be eql to one of:$/) do |set_of_poss|
+  assert_newline_terminated(all_stdout)
   o = parse_output(all_stdout)
   s = parse_output(set_of_poss)
   expect(o).to be_included_with_meql(s)
@@ -23,12 +27,14 @@ end
 
 Then(/^the output should be eql to ruby "(.*?)"$/) do |expr|
   b = eval(expr)
+  assert_newline_terminated(all_stdout)
   a = parse_output(all_stdout)
   expect(a).to be_meql_verbose(b)
 end
 
 Then(/^the output should be eql to ruby:$/) do |expr|
   b = eval(expr)
+  assert_newline_terminated(all_stdout)
   a = parse_output(all_stdout)
   expect(a).to be_meql_verbose(b)
 end
