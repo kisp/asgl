c2a7fec10c5a08a54302bf24f0dd399a6eb0a83c
Kilian Sprotte <kilian.sprotte@gmail.com>
2015-09-24 16:25:53 +0200

    again optimize for speed in gecode.lisp and asgl.lisp

--- gecode.lisp
+++ gecode.lisp
@@ -78,8 +78,8 @@
 
 (in-package :gecode)
 
-(declaim (optimize (debug 3) (safety 3) (speed 0)))
-#+nil(declaim (optimize (debug 0) (safety 1) (speed 3) (space 0)))
+#+nil(declaim (optimize (debug 3) (safety 3) (speed 0)))
+(declaim (optimize (debug 0) (safety 1) (speed 3) (space 0)))
 
 (ffi:clines "#include \"BoolSpace.h\"")
 (ffi:clines "#include \"PrBABSpace.h\"")
