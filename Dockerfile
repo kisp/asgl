e4dae37a9513f8f63127a86453d5bee136cf6256
Kilian Sprotte <kilian.sprotte@gmail.com>
2016-11-15 12:17:44 +0000

    upgrade to ecl-16.1.2

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,8 @@ RUN apt-get -y install git build-essential curl autoconf m4 libgmp-dev ragel bun
 RUN curl --no-progress-bar --retry 10 -o "gecode-4.3.3.tar.gz" \
   -L "http://www.gecode.org/download/gecode-4.3.3.tar.gz"
 
-RUN curl --no-progress-bar --retry 10 -o "ecl-13.5.1.tgz" \
-  -L "http://heanet.dl.sourceforge.net/project/ecls/ecls/13.5/ecl-13.5.1.tgz"
+RUN curl --no-progress-bar --retry 10 -o "ecl-16.1.2.tgz" \
+  -L "https://common-lisp.net/project/ecl/static/files/release/ecl-16.1.2.tgz"
 
 ### gecode
 RUN tar xfz gecode-4.3.3.tar.gz
@@ -23,17 +23,17 @@ RUN cd gecode-4.3.3 && make && make install
 RUN rm -rf gecode-4.3.3*
 
 ### ecl
-RUN tar xfz ecl-13.5.1.tgz
+RUN tar xfz ecl-16.1.2.tgz
 
 ENV ECLFLAGS "-O2 -pipe -fomit-frame-pointer"
 
-RUN cd ecl-13.5.1 && env CFLAGS="$ECLFLAGS" CXXFLAGS="$ECLFLAGS" ./configure \
+RUN cd ecl-16.1.2 && env CFLAGS="$ECLFLAGS" CXXFLAGS="$ECLFLAGS" ./configure \
   --enable-threads --with-cxx --with-dffi=included --enable-boehm=included \
   --with-system-gmp \
   --enable-libatomic=included --enable-unicode --prefix=/usr/local
 
-RUN cd ecl-13.5.1 && make && make install
-RUN rm -rf ecl-13.5.1*
+RUN cd ecl-16.1.2 && make && make install
+RUN rm -rf ecl-16.1.2*
 
 ### asgl
 RUN git clone https://github.com/kisp/asgl.git
--- travis/install.sh
+++ travis/install.sh
@@ -24,10 +24,10 @@ function standard {
     sudo tar -C / -xjf "gecode.tar.bz2"
     rm gecode.tar.bz2
 
-    curl --no-progress-bar --retry 10 -o "ecl.tar.gz" -L "$PACKAGE_HOST/ecl-13.5.1-amd64.tgz"
-    md5sum ecl.tar.gz
-    sudo tar -C / -xzf "ecl.tar.gz"
-    rm ecl.tar.gz
+    curl --no-progress-bar --retry 10 -o "ecl.tar.bz2" -L "$PACKAGE_HOST/ecl-16.1.2-amd64.tar.bz2"
+    md5sum ecl.tar.bz2
+    sudo tar -C / -xf "ecl.tar.bz2"
+    rm ecl.tar.bz2
 
     sudo ldconfig
 
@@ -50,10 +50,10 @@ function dist {
 
     bundle install
 
-    curl --no-progress-bar --retry 10 -o "ecl.tar.gz" -L "$PACKAGE_HOST/ecl-13.5.1-amd64.tgz"
-    md5sum ecl.tar.gz
+    curl --no-progress-bar --retry 10 -o "ecl.tar.gz" -L "$PACKAGE_HOST/ecl-16.1.2-amd64.tar.bz2"
+    md5sum ecl.tar.bz2
     mkdir tmpecl
-    tar --strip-components=2 -C tmpecl -xf ecl.tar.gz
+    tar --strip-components=2 -C tmpecl -xf ecl.tar.bz2
     mv tmpecl/bin/ecl tmpecl/bin/ecl_r
     cat >tmpecl/bin/ecl<<'EOF'
 #!/bin/bash
@@ -64,18 +64,18 @@ fi
 exec ecl_r -dir "$ECL_R_SYS_DIR" "$@"
 EOF
     chmod +x tmpecl/bin/ecl
-    rm ecl.tar.gz
+    rm ecl.tar.bz2
 
     env PATH=`pwd`/tmpecl/bin:$PATH \
         LD_LIBRARY_PATH=`pwd`/tmpecl/lib \
-        ECL_R_SYS_DIR=`pwd`/tmpecl/lib/ecl-13.5.1 \
+        ECL_R_SYS_DIR=`pwd`/tmpecl/lib/ecl-16.1.2 \
         bash scripts/generate-make-mk.sh
 
     cp Makefile.in Makefile
 
     env PATH=`pwd`/tmpecl/bin:$PATH \
         LD_LIBRARY_PATH=`pwd`/tmpecl/lib \
-        ECL_R_SYS_DIR=`pwd`/tmpecl/lib/ecl-13.5.1 \
+        ECL_R_SYS_DIR=`pwd`/tmpecl/lib/ecl-16.1.2 \
         make dist
 
     rm -r tmpecl
