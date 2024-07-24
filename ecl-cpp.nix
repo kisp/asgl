bbeee218276a3d870ee4d3273a8921b1a72de789
Kilian Sprotte <kilian.sprotte@gmail.com>
2024-07-24 07:07:04 +0200

    wip

--- ecl-cpp.nix
+++ ecl-cpp.nix
@@ -89,7 +89,7 @@ stdenv.mkDerivation {
   meta = {
     inherit (s) version;
     description = "Lisp implementation aiming to be small, fast and easy to embed";
-    license = lib.licenses.mit;
+    license = pkgs.lib.licenses.mit;
     maintainers = [ stdenv.lib.maintainers.raskin ];
     platforms = stdenv.lib.platforms.unix;
   };
