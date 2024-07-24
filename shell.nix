7d81649dedbc61bfde32959a291a5359cf2df8d5
Kilian Sprotte <kilian.sprotte@gmail.com>
2024-07-24 08:52:15 +0200

    shell.nix

--- shell.nix
+++ shell.nix
@@ -15,12 +15,15 @@ in
 pkgs.mkShell {
   buildInputs = [
     pkgs.which
+    # For building
     pkgs.autoconf
     ecl_cpp
     gecode
     pkgs.ragel
+    # For tests
     cucumber-aruba-from-gemfile
     pkgs.ruby
+    pkgs.perl
   ];
   shellHook = ''
     echo "Welcome to the development environment!"
